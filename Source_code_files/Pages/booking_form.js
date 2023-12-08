eb6b1e4b9657eb75ebec85752d7e983d72027397
Satwik.Upadhyayula <144178762+SATWIK-SJSU@users.noreply.github.com>
2023-12-08 00:53:06 -0800

    Booking Form

---
+++ Source_code_files/Pages/booking_form.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import "../movie_details.css";
+
+export const Bookingform = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const profile = location.state.profile;
+  const password = location.state.password;
+  const username = location.state.name;
+  const mobile = location.state.mobile;
+  const moviename = location.state.moviename;
+  const ticketcost = location.state.ticketcost;
+  const movieimage = location.state.movieimage;
+  const releasedate = location.state.releasedate;
+  const outdate = location.state.outdate;
+  const email = location.state.email;
+  const premium = location.state.premium;
+  const offer = location.state.offer;
+  var id = location.state.id;
+
+  const [bookingdate, setbookingdate] = useState("");
+  const selectCeats = e => {
+    e.preventDefault();
+    if (
+      username === "" ||
+      moviename === "" ||
+      ticketcost === "" ||
+      bookingdate === ""
+    ) {
+      alert("please select booking date");
+    } else {
+      history.push({
+        pathname: "/pickceat",
+        state: {
+          id: id,
+          offer: offer,
+          profile: profile,
+          email: email,
+          movieimage: movieimage,
+          username: username,
+          mobile: mobile,
+          premium: premium,
+          moviename: moviename,
+          ticketcost: ticketcost,
+          bookingdate: bookingdate,
+          password: password
+        }
+      });
+    }
+  };
+  return (
+    <div class="form-container sign-in-container" className="movie-container">
+      <form style={{ background: "#f6f5f7" }}>
+        <h1>Book Your Ticket</h1>
+        <span>
+          One Ticket Cost : {ticketcost}
+        </span>
+        <br />
+        <input type="text" placeholder="Username" value={username} />
+        <input type="text" placeholder="Movie name" value={moviename} />
+        <input type="text" placeholder="Ticket Amount" value={ticketcost} />
+        <input
+          type="date"
+          placeholder="Pick Booking Date"
+          max={outdate}
+          min={releasedate}
+          value={bookingdate}
+          onChange={e => setbookingdate(e.target.value)}
+        />
+        <br />
+        <button onClick={selectCeats}>Select Seats</button>
+      </form>
+    </div>
+  );
+};
